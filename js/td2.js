c5c9bff7029363a7e1900634c69138836b448be4
garcia-gimenez matthieu <matthieu.garcia-gimenez@etu.univ-cotedazur.fr>
2023-03-29 20:47:43 +0200

    ajout 6.2 et exo 2 quasi-fonctionnel

--- js/td2.js
+++ js/td2.js
@@ -1,68 +1,19 @@
-/**
-* 
-* M413 - TD2
-* * 
-* 	@author Jean-Michel Bruneau
-*	@copyright UCA - IUT -INFO
-* 	@version	1.0
-* 	@date			2021-01-31
-*
-*/
-"use strict";
-
-// M413 - TD2
-let message = 'JavaScript is ok :)';
-// alert( message);
-console.log(message);
-
 function onLoad() {
-	console.log('Processus de chargement du document terminé…');
-	//
-	// All your JavaScript code goes here !
-	//
 
-}
+	document.addEventListener("click", select2);
 
-// Toute les ressources de la page sont complètement chargées.
-window.onload = onLoad;
-
-document.addEventListener("click", select2);
-
-function initSelect(event) {
-	if (event.target.style.backgroundColor == "red") {
-		event.target.style.backgroundColor = null;
-	}
-	else {
-		event.target.style.backgroundColor = "red";
-	}
-}
-
-let ancienEvent = null;
-
-function select(event) {
-	event.target.style.backgroundColor = "blue";
-	if (ancienEvent) {
-		if (ancienEvent.target == event.target) {
-			if (ancienEvent.target.style.backgroundColor == "blue") {
-				event.target.style.backgroundColor = null;
-				ancienEvent = null;
-			}
-			else {
-				ancienEvent = event;
-			}
+	function initSelect(event) {
+		if (event.target.style.backgroundColor == "red") {
+			event.target.style.backgroundColor = null;
 		}
 		else {
-			ancienEvent.target.style.backgroundColor = null;
-			ancienEvent = event;
+			event.target.style.backgroundColor = "red";
 		}
-	} else {
-		ancienEvent = event;
 	}
 
-}
+	let ancienEvent = null;
 
-function select2(event) {
-	if (!event.target.closest("#insert-div")) {
+	function select(event) {
 		event.target.style.backgroundColor = "blue";
 		if (ancienEvent) {
 			if (ancienEvent.target == event.target) {
@@ -81,37 +32,100 @@ function select2(event) {
 		} else {
 			ancienEvent = event;
 		}
+		insertElement(event.target);
+
 	}
+
+	function select2(event) {
+		if (!event.target.closest("#insert-div")) {
+			event.target.style.backgroundColor = "blue";
+			if (ancienEvent) {
+				if (ancienEvent.target == event.target) {
+					if (ancienEvent.target.style.backgroundColor == "blue") {
+						event.target.style.backgroundColor = null;
+						ancienEvent = null;
+					}
+					else {
+						ancienEvent = event;
+					}
+				}
+				else {
+					ancienEvent.target.style.backgroundColor = null;
+					ancienEvent = event;
+				}
+			} else {
+				ancienEvent = event;
+			}
+			//insertElement(event.target);
+		}
+	}
+
+	function insertElement(target) {
+		var insertType = document.getElementById("insert-type").value;
+		var insertText = document.getElementById("insert-text").value;
+		var newElement = document.createElement(insertType);
+		newElement.textContent = insertText;
+		target.parentNode.insertBefore(newElement, target);
+	}
+
+	function search() {
+		var inputRecherche = document.getElementById("inputRecherche");
+		var texteRecherche = inputRecherche.value;
+
+		var nodes = document.body.childNodes;
+		for (var i = 0; i < nodes.length; i++) {
+			var node = nodes[i];
+			if (node.nodeType === Node.TEXT_NODE && node.textContent.includes(texteRecherche)) {
+				var startIndex = node.textContent.indexOf(texteRecherche);
+				var endIndex = startIndex + texteRecherche.length;
+				var beforeText = node.textContent.substring(0, startIndex);
+				var searchText = node.textContent.substring(startIndex, endIndex);
+				var afterText = node.textContent.substring(endIndex);
+				var div = document.createElement("div");
+				span.appendChild(document.createTextNode(beforeText));
+				var mark = document.createElement("mark");
+				mark.style.backgroundColor = "yellow";
+				mark.appendChild(document.createTextNode(searchText));
+				div.appendChild(mark);
+				div.appendChild(document.createTextNode(afterText));
+				node.parentNode.replaceChild(div, node);
+			} else if (node.nodeType === Node.ELEMENT_NODE && node.childNodes.length > 0) {
+				searchRecursive(node, texteRecherche);
+			}
+		}
+	}
+
+	function searchRecursive(node, texteRecherche) {
+		var children = node.childNodes;
+		for (var i = 0; i < children.length; i++) {
+			var child = children[i];
+			if (child.nodeType === Node.TEXT_NODE && child.textContent.includes(texteRecherche)) {
+				var startIndex = child.textContent.indexOf(texteRecherche);
+				var endIndex = startIndex + texteRecherche.length;
+				var beforeText = child.textContent.substring(0, startIndex);
+				var searchText = child.textContent.substring(startIndex, endIndex);
+				var afterText = child.textContent.substring(endIndex);
+				var mark = document.createElement("mark");
+				mark.style.backgroundColor = "yellow";
+				mark.appendChild(document.createTextNode(searchText));
+				var div = document.createElement("div");
+				div.appendChild(document.createTextNode(beforeText));
+				div.appendChild(mark);
+				div.appendChild(document.createTextNode(afterText));
+				child.parentNode.replaceChild(div, child);
+			} else if (child.nodeType === Node.ELEMENT_NODE && child.childNodes.length > 0) {
+				searchRecursive(child, texteRecherche);
+			}
+		}
+	}
+
+
+
+
+
+	var boutonRecherche = document.getElementById("boutonRecherche");
+	boutonRecherche.addEventListener("click", search);
+
 }
 
-function insertElement(target) {
-	const selectElement = document.querySelector('#insert-div select');
-	const selectedType = selectElement.value;
- 
-	const textInput = document.querySelector('#insert-div input[type="text"]');
-	const text = textInput.value;
- 
-	const newElement = document.createElement(selectedType);
-	newElement.textContent = text;
- 
-	target.parentNode.insertBefore(newElement, target);
- }
- 
-
-function search() {
-	var inputRecherche = document.getElementById("inputRecherche");
-	var searchTerm = inputRecherche.value;
-	if (!savedPage) {
-		savedPage = document.body.innerHTML;
-	} else {
-		document.body.innerHTML = savedPage;
-	}
-	var regex = new RegExp(searchTerm, "gi");
-	var bodyHtml = document.body.innerHTML;
-	var highlightedHtml = bodyHtml.replace(regex, match => `<span class="select">${match}</span>`);
-	document.body.innerHTML = highlightedHtml;
-}
-
-var boutonRecherche = document.getElementById("boutonRecherche");
-boutonRecherche.addEventListener("click", search);
-
+window.onload = onLoad;
